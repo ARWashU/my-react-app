980573e57d9777e44411ce3a30dcbfb43682bfb4
Anand Ramabadran <a.ramabadran@wustl.edu>
2025-11-07 12:24:31 -0600

    Initialized handleplay function

--- src/App.jsx
+++ src/App.jsx
@@ -60,6 +60,9 @@ export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const currentSquares = history[history.length - 1];
   
+  function handlePlay(nextSquares) {
+    // TODO
+  }
   return (
     <div className="game">
       <div className="game-board">
