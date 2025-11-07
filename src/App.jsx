6b34c935a3356c9180312802740ec91518426e59
Anand Ramabadran <a.ramabadran@wustl.edu>
2025-11-07 11:06:23 -0600

    Copied Setup From Sandbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App()  {
-  return <h1>Hello World</h1>;
+export default function Square() {
+  return <button className="square">X</button>;
 }
-export default App
