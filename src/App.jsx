08ff44387d0a22cd33b4bb3e8668062b84c82ce3
Anand Ramabadran <a.ramabadran@wustl.edu>
2025-11-07 11:54:49 -0600

    Display the winner and let players know

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,14 @@ function Square({value, onSquareClick}) {
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
-
+  
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
       return;
