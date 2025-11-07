591a5ca54927fc832e3195d084d6623ff3feafc4
Anand Ramabadran <a.ramabadran@wustl.edu>
2025-11-07 11:47:51 -0600

    Forgot to add i from previous step, but added it now

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,13 @@ function Square({ value, onSquareClick }) {
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
-  
-  function handleClick() {
+
+  function handleClick(i) {
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    nextSquares[i] = 'X';
     setSquares(nextSquares);
   }
-  
+
   return (
     <>
       <div className="board-row">
